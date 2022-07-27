2d21eaa3d69f1aaff9049c72f5b0c697c5c70374
Marc Casamitjana i Montseny <mcasamitjana@kavehome.com>
2022-07-27 15:26:54 +0200

    Dynamic form ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import './App.css';
 import DynamicForm from './components/DynamicForm';
+import loginForm from './assets/formFields.json'
+import { Field, FormFields } from './interfaces/form.interface';
+
+enum Store {
+  SPAIN = 'es',
+  KOREA = 'kr',
+  ITALIA = 'it'
+}
+
+const getFormFieldsByStore = (form: FormFields, store: string): Field[] => { 
+  return form.custom ? [...form.generic, ...form.custom[store] || []] : [...form.generic];
+};
+
+const getFieldsOrderIdsByStore = (fields: Field[], fieldsOrderByDefault: string[]): string[] => {
+  const fieldIds: string[] = fields.map(field => field.id);
+  return fieldsOrderByDefault.filter((orderId: string) => fieldIds.includes(orderId));
+}
+
+const sortFieldsByStore = (fields: Field[], fieldsOrderIds: any): Field[] => {
+  const order: any = {};
+  fieldsOrderIds.forEach((fieldId, i) => order[fieldId] = i);
+  return fields.sort((a, b) => order[a.id] - order[b.id]);
+}
 
 function App() {
+  const actualStore: Store = Store.ITALIA;
+  const fields: Field[] = getFormFieldsByStore(loginForm, actualStore);
+  const fieldsOrderIds: string[] = getFieldsOrderIdsByStore(fields, loginForm.fieldsOrder);
+  const sortedFields: Field[] = sortFieldsByStore(fields, fieldsOrderIds);
+
+  const onSubmit = (value: any) => console.log(value);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-      <DynamicForm />
-      </header>
+    <div>
+      <DynamicForm {...{fields: sortedFields, onSubmit}}/>
     </div>
   );
 }
--- src/components/DynamicForm/index.tsx
+++ src/components/DynamicForm/index.tsx
@@ -1,5 +1,32 @@
-function DynamicForm() : JSX.Element {
-    return <h1>Dynamic Form</h1>
+import { Field } from "../../interfaces/form.interface";
+
+interface FormFieldProps {
+    field: Field;
+}
+
+function FormField({ field }: FormFieldProps): JSX.Element {
+    const {id, label, placeholder, type, value, validationType, validations} = field;
+    return <div style={{paddingTop: 5}}>
+        <label htmlFor={id} style={{paddingRight: 20}}>{label}:</label>
+        <input type={type} id={id} name="fname" placeholder={placeholder}></input><br />
+    </div>;
+}
+
+interface DynamicFormProps {
+    fields: any[];
+    onSubmit: (value: any) => void;
+}
+
+function DynamicForm({ fields, onSubmit}: DynamicFormProps) : JSX.Element {
+    return (
+        <form onSubmit={onSubmit}>
+            {
+               fields.map((field: Field) => <FormField key={field.id} {...{field}} />) 
+            }
+            <br />
+            <input type="submit" value="Guardar" />
+        </form>
+    )
 }
 
 export default DynamicForm;
